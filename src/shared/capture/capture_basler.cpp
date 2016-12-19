e473f2223a3646405f735d9287f0bacd3119dc6d
Satyanand <greptruth@gmail.com>
2016-12-19 20:42:56 +0530

    Plylon cmake includes not working

--- src/shared/capture/capture_basler.cpp
+++ src/shared/capture/capture_basler.cpp
@@ -33,6 +33,7 @@ CaptureBasler::CaptureBasler ( VarList * _settings, int default_camera_id) : Cap
 
   settings->addChild ( conversion_settings = new VarList ( "Conversion Settings" ) );
   settings->addChild ( capture_settings = new VarList ( "Capture Settings" ) );
+  settings->addChild(dcam_parameters  = new VarList("Camera Parameters"));
 
   //=======================CONVERSION SETTINGS=======================
   conversion_settings->addChild ( v_colorout=new VarStringEnum ( "convert to mode",Colors::colorFormatToString ( COLOR_YUV422_UYVY ) ) );
@@ -40,10 +41,97 @@ CaptureBasler::CaptureBasler ( VarList * _settings, int default_camera_id) : Cap
   v_colorout->addItem ( Colors::colorFormatToString ( COLOR_YUV422_UYVY ) );
 
   //=======================CAPTURE SETTINGS==========================
+  capture_settings->addChild(v_cam_bus          = new VarInt("cam idx",default_camera_id));
+  capture_settings->addChild(v_colorout         = new VarStringEnum("color mode", Colors::colorFormatToString(COLOR_YUV422_UYVY)));
+  v_colorout->addItem(Colors::colorFormatToString(COLOR_YUV422_UYVY));
+  v_colorout->addItem(Colors::colorFormatToString(COLOR_RGB8));
   capture_settings->addChild ( v_framerate = new VarDouble ( "Framerate (FPS)", 60.0 ) );
   capture_settings->addChild ( v_width = new VarInt ( "Width (pixels)", 780 ) );
   capture_settings->addChild ( v_height = new VarInt ( "Height (pixels)", 580 ) );
   capture_settings->addChild ( v_test_image = new VarBool ( "Generate Color Test Image", false ) );
+
+  //=======================CAMERA PARAMETERS=======================
+  // v_aoi_width = new VarInt("AOI width", 752, 0, 752);
+  dcam_parameters->addChild(v_aoi_width = new VarInt("AOI width", 752, 0, 752) );
+  dcam_parameters->addChild(v_aoi_height = new VarInt("AOI height", 480, 0, 480) );
+  dcam_parameters->addChild(v_aoi_left = new VarInt("AOI left", 0, 0, 752) );
+  dcam_parameters->addChild(v_aoi_top = new VarInt("AOI top", 0, 0, 480) );
+  dcam_parameters->addChild(v_gain_db = new VarDouble("Gain [dB]", 0.0, 0.0, 12.0) );
+  // dcam_parameters->addChild(v_pixel_clock);
+  // dcam_parameters->addChild(v_expose_us);
+  // dcam_parameters->addChild(v_expose_overlapped);
+  // dcam_parameters->addChild(v_hdr_mode);
+  // dcam_parameters->addChild(v_mirror_top_down);
+  // dcam_parameters->addChild(v_mirror_left_right);
+  // dcam_parameters->addChild(v_wb_red);
+  // dcam_parameters->addChild(v_wb_green);
+  // dcam_parameters->addChild(v_wb_blue);
+  // dcam_parameters->addChild(v_sharpen);
+  // dcam_parameters->addChild(v_gamma);
+  // dcam_parameters->addChild(v_color_twist_mode);
+
+  
+  
+
+
+  
+  #ifndef VDATA_NO_QT
+    mvc_connect(dcam_parameters);
+    mutex.unlock();
+  #endif
+  
+}
+
+#ifndef VDATA_NO_QT
+void CaptureBasler::mvc_connect(VarList * group)
+{
+  vector<VarType *> v=group->getChildren();
+  for (unsigned int i=0;i<v.size();i++)
+  {
+    connect(v[i],SIGNAL(wasEdited(VarType *)),group,SLOT(mvcEditCompleted()));
+  }
+  connect(group,SIGNAL(wasEdited(VarType *)),this,SLOT(changed(VarType *)));
+}
+
+void CaptureBasler::changed(VarType * group)
+{
+  if (group->getType()==VARTYPE_ID_LIST)
+  {
+    writeParameterValues( (VarList *)group );
+    readParameterValues( (VarList *)group );
+  }
+}
+#endif
+
+void CaptureBasler::readAllParameterValues()
+{
+  readParameterValues(dcam_parameters);
+}
+
+void CaptureBasler::writeAllParameterValues()
+{
+  writeParameterValues(dcam_parameters);
+}
+
+void CaptureBasler::readParameterValues(VarList * item)
+{
+  if(item != dcam_parameters)
+    return;
+  
+  #ifndef VDATA_NO_QT
+    mutex.lock();
+  #endif
+    
+    // TODO: could do a read-out, but why?
+//   v_expose_us->setInt(pSettings->expose_us.read());
+    
+  #ifndef VDATA_NO_QT
+    mutex.unlock();
+  #endif
+}
+void CaptureBasler::writeParameterValues(VarList * item)
+{
+  //TODO" writeParameterValues(0)
 }
 
 CaptureBasler::~CaptureBasler()
@@ -58,135 +146,135 @@ bool CaptureBasler::stopCapture()
 
 void CaptureBasler::cleanup()
 {
-#ifndef VDATA_NO_QT
-  mutex.lock();
-#endif
-  is_capturing=false;
-#ifndef VDATA_NO_QT
-  mutex.unlock();
-#endif
+  #ifndef VDATA_NO_QT
+    mutex.lock();
+  #endif
+    // is_capturing=false;
+  #ifndef VDATA_NO_QT
+    mutex.unlock();
+  #endif
 }
 
 bool CaptureBasler::startCapture()
 {
-#ifndef VDATA_NO_QT
-  mutex.lock();
-#endif
-  limit.init ( v_framerate->getDouble() );
-  is_capturing=true;
+  #ifndef VDATA_NO_QT
+    mutex.lock();
+  #endif
 
+  
+      
 
-#ifndef VDATA_NO_QT
-  mutex.unlock();
-#endif
-  return true;
+  #ifndef VDATA_NO_QT
+    mutex.unlock();
+  #endif
+    return true;
 }
 
 bool CaptureBasler::copyAndConvertFrame ( const RawImage & src, RawImage & target )
 {
-#ifndef VDATA_NO_QT
-  mutex.lock();
-#endif
-  ColorFormat output_fmt = Colors::stringToColorFormat ( v_colorout->getSelection().c_str() );
-  ColorFormat src_fmt=src.getColorFormat();
+  #ifndef VDATA_NO_QT
+    mutex.lock();
+  #endif
+  //   ColorFormat output_fmt = Colors::stringToColorFormat ( v_colorout->getSelection().c_str() );
+  //   ColorFormat src_fmt=src.getColorFormat();
 
-  if ( target.getData() ==0 ) {
-    target.allocate ( output_fmt, src.getWidth(), src.getHeight() );
-  } else {
-    target.ensure_allocation ( output_fmt, src.getWidth(), src.getHeight() );
-  }
-  target.setTime ( src.getTime() );
+  //   if ( target.getData() ==0 ) {
+  //     target.allocate ( output_fmt, src.getWidth(), src.getHeight() );
+  //   } else {
+  //     target.ensure_allocation ( output_fmt, src.getWidth(), src.getHeight() );
+  //   }
+  //   target.setTime ( src.getTime() );
 
-  if ( output_fmt == src_fmt ) {
-    if ( src.getData() != 0 ) memcpy ( target.getData(),src.getData(),src.getNumBytes() );
-  } else if ( src_fmt == COLOR_RGB8 && output_fmt == COLOR_YUV422_UYVY ) {
-    if ( src.getData() != 0 ) {
-      dc1394_convert_to_YUV422 ( src.getData(), target.getData(), src.getWidth(), src.getHeight(),
-                                 DC1394_BYTE_ORDER_UYVY, DC1394_COLOR_CODING_RGB8, 8 );
-    }
-  } else {
-    fprintf ( stderr,"Cannot copy and convert frame...unknown conversion selected from: %s to %s\n",
-              Colors::colorFormatToString ( src_fmt ).c_str(),
-              Colors::colorFormatToString ( output_fmt ).c_str() );
-#ifndef VDATA_NO_QT
+  //   if ( output_fmt == src_fmt ) {
+  //     if ( src.getData() != 0 ) memcpy ( target.getData(),src.getData(),src.getNumBytes() );
+  //   } else if ( src_fmt == COLOR_RGB8 && output_fmt == COLOR_YUV422_UYVY ) {
+  //     if ( src.getData() != 0 ) {
+  //       dc1394_convert_to_YUV422 ( src.getData(), target.getData(), src.getWidth(), src.getHeight(),
+  //                                  DC1394_BYTE_ORDER_UYVY, DC1394_COLOR_CODING_RGB8, 8 );
+  //     }
+  //   } else {
+  //     fprintf ( stderr,"Cannot copy and convert frame...unknown conversion selected from: %s to %s\n",
+  //               Colors::colorFormatToString ( src_fmt ).c_str(),
+  //               Colors::colorFormatToString ( output_fmt ).c_str() );
+  // #ifndef VDATA_NO_QT
+  //     mutex.unlock();
+  // #endif
+  //     return false;
+  //   }
+  #ifndef VDATA_NO_QT
     mutex.unlock();
-#endif
-    return false;
-  }
-#ifndef VDATA_NO_QT
-  mutex.unlock();
-#endif
-  return true;
+  #endif
+    return true;
 }
 
 RawImage CaptureBasler::getFrame()
 {
-#ifndef VDATA_NO_QT
-  mutex.lock();
-#endif
-  limit.waitForNextFrame();
-  result.setColorFormat ( COLOR_RGB8 );
-  result.setTime ( GetTimeSec() );
-  result.allocate ( COLOR_RGB8,v_width->getInt(),v_height->getInt() );
-  rgbImage img;
-  img.fromRawImage(result);
+  #ifndef VDATA_NO_QT
+    mutex.lock();
+  #endif
+    // limit.waitForNextFrame();
+    // result.setColorFormat ( COLOR_RGB8 );
+    // result.setTime ( GetTimeSec() );
+    // result.allocate ( COLOR_RGB8,v_width->getInt(),v_height->getInt() );
+    // rgbImage img;
+    // img.fromRawImage(result);
 
-  if (v_test_image->getBool()) {
-    int w = result.getWidth();
-    int h = result.getHeight();
-    int n_colors = 8;
-    int slice_width = w/n_colors;
-    rgb color=RGB::Black;
-    rgb color2;
-    for (int x = 0 ; x < w; x++) {
-      int c_idx= x/slice_width;
-      if (c_idx==0) {
-        color=RGB::White;
-      } else if (c_idx==1) {
-        color=RGB::Red;
-      } else if (c_idx==2) {
-        color=RGB::Green;
-      } else if (c_idx==3) {
-        color=RGB::Blue;
-      } else if (c_idx==4) {
-        color=RGB::Cyan;
-      } else if (c_idx==5) {
-        color=RGB::Pink;
-      } else if (c_idx==6) {
-        color=RGB::Yellow;
-      } else if (c_idx==7) {
-        color=RGB::Black;
-      }
-      for (int y = 0 ; y < h; y++) {
-        color2.r= max(0,min(255,((int)color.r * (h-1-y))/h));
-        color2.g= max(0,min(255,((int)color.g * (h-1-y))/h));
-        color2.b= max(0,min(255,((int)color.b * (h-1-y))/h));
-        img.setPixel(x,y,color2);
-      }
-    }
-  } else {
-    img.fillBlack();
-  }
+    // if (v_test_image->getBool()) {
+    //   int w = result.getWidth();
+    //   int h = result.getHeight();
+    //   int n_colors = 8;
+    //   int slice_width = w/n_colors;
+    //   rgb color=RGB::Black;
+    //   rgb color2;
+    //   for (int x = 0 ; x < w; x++) {
+    //     int c_idx= x/slice_width;
+    //     if (c_idx==0) {
+    //       color=RGB::White;
+    //     } else if (c_idx==1) {
+    //       color=RGB::Red;
+    //     } else if (c_idx==2) {
+    //       color=RGB::Green;
+    //     } else if (c_idx==3) {
+    //       color=RGB::Blue;
+    //     } else if (c_idx==4) {
+    //       color=RGB::Cyan;
+    //     } else if (c_idx==5) {
+    //       color=RGB::Pink;
+    //     } else if (c_idx==6) {
+    //       color=RGB::Yellow;
+    //     } else if (c_idx==7) {
+    //       color=RGB::Black;
+    //     }
+    //     for (int y = 0 ; y < h; y++) {
+    //       color2.r= max(0,min(255,((int)color.r * (h-1-y))/h));
+    //       color2.g= max(0,min(255,((int)color.g * (h-1-y))/h));
+    //       color2.b= max(0,min(255,((int)color.b * (h-1-y))/h));
+    //       img.setPixel(x,y,color2);
+    //     }
+    //   }
+    // } else {
+    //   img.fillBlack();
+    // }
 
 
-#ifndef VDATA_NO_QT
-  mutex.unlock();
-#endif
-  return result;
+  #ifndef VDATA_NO_QT
+    mutex.unlock();
+  #endif
+    return result;
 }
 
 void CaptureBasler::releaseFrame()
 {
-#ifndef VDATA_NO_QT
-  mutex.lock();
-#endif
-
-#ifndef VDATA_NO_QT
-  mutex.unlock();
-#endif
+  #ifndef VDATA_NO_QT
+    mutex.lock();
+  #endif
+      //TODO release frame here
+  #ifndef VDATA_NO_QT
+    mutex.unlock();
+  #endif
 }
 
 string CaptureBasler::getCaptureMethodName() const
 {
-  return "Image Basler";
+  return "Basler";
 }
--- src/shared/capture/capture_basler.h
+++ src/shared/capture/capture_basler.h
@@ -33,6 +33,17 @@
 #else
   #include <pthread.h>
 #endif
+// Include files to use the PYLON API.
+#include <pylon/PylonIncludes.h>
+#ifdef PYLON_WIN_BUILD
+#    include <pylon/PylonGUI.h>
+#endif
+// Namespace for using pylon objects.
+using namespace Pylon;
+// Namespace for using cout.
+// using namespace std;
+// Number of images to be grabbed.
+static const uint32_t c_countOfImagesToGrab = 10;
 
 
 #ifndef VDATA_NO_QT
@@ -45,8 +56,8 @@ class CaptureBasler : public CaptureInterface
 {
 #ifndef VDATA_NO_QT
   Q_OBJECT
-/*   public slots: */
-/*   void changed(VarType * group); */
+   public slots: 
+   void changed(VarType * group); 
   protected:
   QMutex mutex;
   public:
@@ -66,6 +77,161 @@ protected:
   VarInt * v_height;
   VarDouble * v_framerate;
   VarBool * v_test_image;
+
+  // enum HDRMode
+  // {
+  //   HDR_MODE_OFF,
+  //   HDR_MODE_FIXED0,
+  //   HDR_MODE_FIXED1,
+  //   HDR_MODE_FIXED2,
+  //   HDR_MODE_FIXED3,
+  //   HDR_MODE_FIXED4,
+  //   HDR_MODE_FIXED5,
+  // };
+  
+  // static HDRMode stringToHdrMode(const char* s)
+  // {
+  //   if (strcmp(s,"Off")==0) {
+  //     return HDR_MODE_OFF;
+  //   } else if (strcmp(s,"Fixed0")==0) {
+  //     return HDR_MODE_FIXED0;
+  //   } else if (strcmp(s,"Fixed1")==0) {
+  //     return HDR_MODE_FIXED1;
+  //   } else if (strcmp(s,"Fixed2")==0) {
+  //     return HDR_MODE_FIXED2;
+  //   } else if (strcmp(s,"Fixed3")==0) {
+  //     return HDR_MODE_FIXED3;
+  //   } else if (strcmp(s,"Fixed4")==0) {
+  //     return HDR_MODE_FIXED4;
+  //   } else if (strcmp(s,"Fixed5")==0) {
+  //     return HDR_MODE_FIXED5;
+  //   } else {
+  //     return HDR_MODE_OFF;
+  //   }
+  // }
+  
+  // static string hdrModeToString(HDRMode mode)
+  // {
+  //   switch(mode)
+  //   {
+  //     case HDR_MODE_FIXED0: return "Fixed0";
+  //     case HDR_MODE_FIXED1: return "Fixed1";
+  //     case HDR_MODE_FIXED2: return "Fixed2";
+  //     case HDR_MODE_FIXED3: return "Fixed3";
+  //     case HDR_MODE_FIXED4: return "Fixed4";
+  //     case HDR_MODE_FIXED5: return "Fixed5";
+  //     default: return "Off";
+  //   }
+  // }
+  
+  // enum ColorTwistMode
+  // {
+  //   COLOR_TWIST_MODE_OFF,
+  //   COLOR_TWIST_MODE_ADOBERGB_D50,
+  //   COLOR_TWIST_MODE_SRGB_D50,
+  //   COLOR_TWIST_MODE_WIDE_GAMUT_RGB_D50,
+  //   COLOR_TWIST_MODE_ADOBERGB_D65,
+  //   COLOR_TWIST_MODE_SRGB_D65,
+  // };
+  
+  // static ColorTwistMode stringToColorTwistMode(const char* s)
+  // {
+  //     if (strcmp(s,"Off")==0) {
+  //     return COLOR_TWIST_MODE_OFF;
+  //   } else if (strcmp(s,"AdobeRGB_D50")==0) {
+  //     return COLOR_TWIST_MODE_ADOBERGB_D50;
+  //   } else if (strcmp(s,"sRGB_D50")==0) {
+  //     return COLOR_TWIST_MODE_SRGB_D50;
+  //   } else if (strcmp(s,"WideGamutRGB_D50")==0) {
+  //     return COLOR_TWIST_MODE_WIDE_GAMUT_RGB_D50;
+  //   } else if (strcmp(s,"AdobeRGB_D65")==0) {
+  //     return COLOR_TWIST_MODE_ADOBERGB_D65;
+  //   } else if (strcmp(s,"sRGB_D65")==0) {
+  //     return COLOR_TWIST_MODE_SRGB_D65;
+  //   } else {
+  //     return COLOR_TWIST_MODE_OFF;
+  //   }
+  // }
+  
+  // static string colorTwistModeToString(ColorTwistMode mode)
+  // {
+  //   switch(mode)
+  //   {
+  //     case COLOR_TWIST_MODE_ADOBERGB_D50: return "AdobeRGB_D50";
+  //     case COLOR_TWIST_MODE_SRGB_D50: return "sRGB_D50";
+  //     case COLOR_TWIST_MODE_WIDE_GAMUT_RGB_D50: return "WideGamutRGB_D50";
+  //     case COLOR_TWIST_MODE_ADOBERGB_D65: return "AdobeRGB_D65";
+  //     case COLOR_TWIST_MODE_SRGB_D65: return "sRGB_D65";
+  //     default: return "Off";
+  //   }
+  // }
+
+
+  // static TCameraPixelClock stringToPixelClock(const char* s)
+  // {
+  //   if (strcmp(s,"8MHz")==0) {
+  //     return cpc8000KHz;
+  //   } else if (strcmp(s,"10MHz")==0) {
+  //     return cpc10000KHz;
+  //   } else if (strcmp(s,"12MHz")==0) {
+  //     return cpc12000KHz;
+  //   } else if (strcmp(s,"20MHz")==0) {
+  //     return cpc20000KHz;
+  //   } else if (strcmp(s,"24MHz")==0) {
+  //     return cpc24000KHz;
+  //   } else if (strcmp(s,"27MHz")==0) {
+  //     return cpc27000KHz;
+  //   } else if (strcmp(s,"32MHz")==0) {
+  //     return cpc32000KHz;
+  //   } else if (strcmp(s,"40MHz")==0) {
+  //     return cpc40000KHz;
+  //   } else {
+  //     return cpc6000KHz;
+  //   }
+  // }
+
+  // static string pixelClockToString(TCameraPixelClock pxc)
+  // {
+  //   switch(pxc)
+  //   {
+  //     case cpc8000KHz: return "8MHz";
+  //     case cpc10000KHz: return "10MHz";
+  //     case cpc12000KHz: return "12MHz";
+  //     case cpc20000KHz: return "20MHz";
+  //     case cpc24000KHz: return "24MHz";
+  //     case cpc27000KHz: return "27MHz";
+  //     case cpc32000KHz: return "32MHz";
+  //     case cpc40000KHz: return "40MHz";
+  //     default: return "6MHz";
+  //   }
+  // }
+   // bool is_capturing;
+
+  //DCAM parameters:
+  VarInt* v_expose_us;
+  VarBool* v_expose_overlapped;
+  VarDouble* v_gain_db;
+  VarStringEnum* v_hdr_mode;
+  VarBool* v_mirror_top_down;
+  VarBool* v_mirror_left_right;
+  VarDouble* v_wb_red;
+  VarDouble* v_wb_green;
+  VarDouble* v_wb_blue;
+  VarBool* v_sharpen;
+  VarDouble* v_gamma;
+  VarStringEnum* v_color_twist_mode;
+  VarInt    * v_aoi_width;
+  VarInt    * v_aoi_height;
+  VarInt    * v_aoi_left;
+  VarInt    * v_aoi_top;
+  VarStringEnum* v_pixel_clock;
+  
+  //capture variables:
+  VarInt    * v_cam_bus;
+  // VarStringEnum * v_colorout;
+  
+  VarList * dcam_parameters;
+  // VarList * capture_settings;
   
 public:
 #ifndef VDATA_NO_QT
@@ -84,6 +250,14 @@ public:
   virtual void releaseFrame();
    
   void cleanup();
+
+  void readParameterValues(VarList * item);
+
+  void writeParameterValues(VarList * item);
+
+  virtual void readAllParameterValues();
+
+  void writeAllParameterValues();
 
   virtual bool copyAndConvertFrame(const RawImage & src, RawImage & target);
   virtual string getCaptureMethodName() const;
